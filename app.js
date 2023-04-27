ceb014fd3e0a672690ff8d905bf6cf54f07dbc16
Robert <rlhead94@gmail.com>
2023-04-27 17:12:16 -0400

    petshop presentation

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var shop = [
     {
       title: 'Leash',
       image: 'https://cdn.shopify.com/s/files/1/1577/4333/products/40354-Roamer-Leash-Granite-Gray.png?crop=center&height=550&v=1676490728&width=820',
-      price: '$5.00',
+      price: '5.00',
       description:'Our top selling dog accesory'
     },
     {
         title: 'Water Dish',
         image: 'https://cdn1.bigcommerce.com/server700/6bee4/products/441/images/1852/Tusco_Dish_Round__77789.1552409525.1280.1280.jpg?c=2',
-        price: '$15.00',
+        price: '15.00',
         description:'Perfect for any pet'
       },
     
